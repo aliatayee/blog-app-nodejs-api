c40d004cc398ded874f38a466920daa7008c13df
aliatayee <aliaqaatayee@gmail.com>
2022-11-22 16:54:05 +0430

    Add jwt to protect routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,35 +1,63 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
 
 //Register
 router.post("/register", async (req, res) => {
     try {
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-        const newUser = new User({
-            username: req.body.username,
-            email: req.body.email,
-            password: hashedPassword,
+        // Get user input
+        const { username, email, password } = req.body;
+        if (!(email && password && username)) {
+            res.status(400).send("All input is required");
+        }
+        const oldEmail = await User.findOne({ email });
+        const oldUsername = await User.findOne({ username });
+        if (oldEmail) {
+            return res.status(409).send(`${email} already exist`);
+        }
+        if (oldUsername) {
+            return res.status(409).send(`${username} already exist`);
+        }
+        encryptedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            username,
+            email: email.toLowerCase(),
+            password: encryptedPassword,
         });
-        const user = await newUser.save();
-        res.status(200).json(user);
+        // Create token
+        const token = jwt.sign({ user_id: user._id, email }, 'secret', { expiresIn: "2h", });
+        const data = { ...user._doc }
+        data.token = token;
+        res.status(200).json(data);
     } catch (err) {
-        res.status(500).json(err);
+        console.log(err);
     }
+
 });
 
 // Login
 router.post("/login", async (req, res) => {
     try {
-        const user = await User.findOne({ username: req.body.username })
-        !user && res.status(400).json("Wrong credentials");
-        const validate = await bcrypt.compare(req.body.password, user.password)
-        !validate && res.status(400).json("Wrong credentials");
-        const { password, ...details } = user._doc
-        res.status(200).json(details);
+        const { username, password } = req.body;
+        // Validate user input
+        if (!(username && password)) {
+            res.status(400).send("All input is required");
+        }
+        // Validate if user exist in our database
+        const user = await User.findOne({ username });
+        if (user && (await bcrypt.compare(password, user.password))) {
+            // Create token
+            const token = jwt.sign({ user_id: user._id, email: user._email }, 'secret', { expiresIn: "2h", });
+            const data = { ...user._doc }
+            data.token = token;
+            res.status(200).json(data);
+        } else {
+            res.status(400).send("Invalid Credentials");
+        }
+
     } catch (err) {
-        res.status(500).json(err);
+        console.log(err);
     }
 })
 module.exports = router; 
