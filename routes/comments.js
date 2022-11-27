f8e4866aeb4fbe3446c58f007101dc185644c25e
aliatayee <aliaqaatayee@gmail.com>
2022-11-27 16:29:28 +0430

    Add comment route

---
+++ routes/comments.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const Category = require("../models/Comment");
+const auth = require("../middleware/auth")
+//GET CATEGORIES
+
+//CREATE CATEGORY
+
+
+//UPDATE CATEGORY
+
+
+//DELETE CATEGORY
+
+module.exports = router; 
